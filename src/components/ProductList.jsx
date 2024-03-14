2df0c4606f8cabc2076d972bc9dc5b68620f1709
Bhargav5427 <vadodariyabhargav54@gmail.com>
2024-03-14 09:14:57 +0530

    product-Shorting

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -96,13 +96,13 @@ const ProductList = () => {
         <>
             <div className='inputfields row col-12 mb-5'>
 
-                <div className='col-4'>
+                <div className='col-6'>
                     <h1>Add products</h1>
                     <input type="text" placeholder='Product Name' ref={name} />
                     <input type="text" placeholder='Price' ref={price} />
-                    <button className='btn btn-info' onClick={addProduct}>Add</button>
+                    <button className='btn btn-info' onClick={addProduct} style={{width:"100%"}}>Add</button>
                 </div>
-                <div className='col-4'>
+                <div className='col-6'>
                     <h1>Product Dashboard</h1>
                     <input
                         type="text"
@@ -114,12 +114,6 @@ const ProductList = () => {
                         <option value="name">Sort by Name</option>
                         <option value="price">Sort by Price</option>
                     </select>
-                </div>
-                <div className='col-4'>
-                    <h1>Update products</h1>
-                    <input type="text" value={update.name} placeholder='Product Name' name='name' onChange={updateHandler} />
-                    <input type="text" value={update.price} placeholder='Price' name='price' onChange={updateHandler} />
-                    <button className='btn btn-info' onClick={updateProduct}>Update</button>
                 </div>
             </div> <table class="table table-bordered text-center">
                 <thead>
@@ -136,23 +130,37 @@ const ProductList = () => {
                         filteredProducts.map((product, index) => {
                             return (
                                 <>
-                                    {/* <li className='row justify-content-between' key={product.id}>{product.name} - ${product.price}
-                                    <div>
-                                        <button className='mr-3 btn btn-primary' onClick={() => viewdata(product.id)}>Edit</button>
-                                        <button className='btn btn-outline-danger' onClick={() => deleteProduct(product.id)}>Delete</button>
-                                    </div>
-                                </li> */}
 
 
                                     <tr>
                                         <th scope="row">{product.id}</th>
                                         <td>{product.name}</td>
                                         <td>${product.price}</td>
-                                        <td style={{ width: '75px' }}><button className='btn btn-primary' onClick={() => viewdata(product.id)}>Edit</button>
+                                        <td style={{ width: '75px' }}><button className='btn btn-primary' data-toggle="modal" data-target="#exampleModal" onClick={() => viewdata(product.id)}>Edit</button>
                                         </td>
-                                        <td style={{width:'100px'}}><button className='btn btn-outline-danger' onClick={() => deleteProduct(product.id)}>Delete</button>
+                                        <td style={{ width: '100px' }}><button className='btn btn-outline-danger' onClick={() => deleteProduct(product.id)}>Delete</button>
                                         </td>
                                     </tr>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <input type="text" value={update.name} placeholder='Product Name' name='name' onChange={updateHandler} />
+                                                    <input type="text" value={update.price} placeholder='Price' name='price' onChange={updateHandler} />
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary" data-dismiss="modal"  onClick={updateProduct}>Update</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </>
                             )
                         })
